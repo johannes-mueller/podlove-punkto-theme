6e697d2de6a09ff95cd6e79509dd1222b4039c74
Johannes Mueller <github@johannes-mueller.org>
2015-12-12 14:12:06 +0100

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( "— four-O-four — not found — 404", 'punktoinfo' ); ?></h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<h2><?php _e( 'We don&rsquo;t know who has taken you here.', 'punktoinfo' ); ?></h2>
+					<p><?php _e( 'Maybe you clicked on a wrong or obselete link?<br>Or typed in a wrong address?', 'punktoinfo' ); ?></p>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
