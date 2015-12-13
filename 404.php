143ec81e660526e01b97c27fbafad21a4de68bed
Johannes Mueller <github@johannes-mueller.org>
2015-12-13 23:24:57 +0100

    Don't use page-content anymore

--- 404.php
+++ 404.php
@@ -12,16 +12,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( "— four-O-four — not found — 404", 'punktoinfo' ); ?></h1>
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( "— four-O-four — not found — 404", 'punktoinfo' ); ?></h1>
 			</header>
 
-			<div class="page-wrapper">
-				<div class="page-content">
-					<h2><?php _e( 'We don&rsquo;t know who has taken you here.', 'punktoinfo' ); ?></h2>
-					<p><?php _e( 'Maybe you clicked on a wrong or obsolete link?<br>Or typed in a wrong address?', 'punktoinfo' ); ?></p>
-				</div><!-- .page-content -->
-			</div><!-- .page-wrapper -->
+			<div class="entry-content">
+				<h2><?php _e( 'We don&rsquo;t know who has taken you here.', 'punktoinfo' ); ?></h2>
+				<p><?php _e( 'Maybe you clicked on a wrong or obsolete link?<br>Or typed in a wrong address?', 'punktoinfo' ); ?></p>
+			</div><!-- .entry-content -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
