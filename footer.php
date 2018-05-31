d9987c205ebf59837b35d7f4c2e5245d6d2f7606
Johannes Mueller <github@johannes-mueller.org>
2018-05-31 13:11:41 +0200

    aldonis datumprotektan deklaron

--- footer.php
+++ footer.php
@@ -17,8 +17,8 @@
 		</div>
 	<?php endif ?>
         <div class="site-info">
-		Fiere farite per Worpdress kun memhakita etoso. Kodo prunteprenita de la etoso 2013.<br>
-		La enhavo de tiu ĉi retejo estas disponebla laŭ la permesilo Krea Komunaĵo Atribuite-Samkondiĉe 4.0 Tutmonda. Ni petas, ke vi informu nin, kiam vi uzas la materialon por ia celo. Des pli ni ĝojus, ke nia laboro estas utila. <img src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png">
+		Fiere farite per Worpdress kun memhakita etoso. Kodo prunteprenita de la etoso 2013. <a href="https://johannes-mueller.org/datumprotektado.html">Datumprotekado</a><br>
+		La enhavo de tiu ĉi retejo estas disponebla laŭ la permesilo Krea Komunaĵo Atribuite-Samkondiĉe 4.0 Tutmonda. Ni petas, ke vi informu nin, kiam vi uzas la materialon por ia celo. Des pli ni ĝojus, ke nia laboro estas utila. <img src="/img/cc-by-sa.png">
           </div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
